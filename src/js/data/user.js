fb4dfe19d6c04239fd5199bd3aa6bfeafdce583e
LevelLawrence-FS <lhlawrence@student.fullsail.edu>
2023-11-16 11:53:54 -0500

    heroku server test

---
+++ src/js/data/user.js
@@ -0,0 +1,17 @@
+import { faker } from "@faker-js/faker";
+
+const avatar = faker.image.avatar();
+const name = faker.person.firstName();
+const email = faker.internet.email();
+const latitude = faker.location.latitude();
+const longitude = faker.location.longitude();
+
+const userProfile = {
+  avatar,
+  name,
+  email,
+  latitude,
+  longitude,
+};
+
+export { userProfile };
